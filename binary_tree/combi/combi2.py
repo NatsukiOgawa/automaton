9426c36b3c361e0483a4dafa9298557f38fc940e
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2019-08-23 20:26:38 +0900

    add some codes

--- binary_tree/combi/combi2.py
+++ binary_tree/combi/combi2.py
@@ -71,89 +71,93 @@ for n in range(1):
             for k in range(0, three_num):
                 for j in range(0, four_num):
                     for i in range(0, five_num):
-                        print("=====")
-                        print("=====")
-                        print("====")
-                        print("===")
-                        print("==")
-                        print("=")
+                        # print("=====")
+                        # print("=====")
+                        # print("====")
+                        # print("===")
+                        # print("==")
+                        # print("=")
                         print("{}{}{}{}{}".format(path_list[n][m], path_list[m][l],path_list[l][k], path_list[k][j], path_list[j][i]))
                         print("{}".format(one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]))
                         total_reward_list[reward_index_counter] = one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]
+                        best_path = np.argmax(total_reward_list)
+                        best_i_j_k_l_m = [i, j, k, l, m]
+                        print(best_path)
+                        print(best_i_j_k_l_m)
                         reward_index_counter += 1
 # print(one_list)
 # print(two_list)
 # print(three_list)
 # print(four_list)
 # print(five_list)
-print("=========================================================================================================================================")
-print("報酬の添字(終了時：one_num * two_num + three_num * four_num * five_num)")
-print(reward_index_counter)
-# print(total_reward_list)
-print()
-print()
-print("best_path(インデックス)")
-best_path = np.argmax(total_reward_list)
-print(best_path)
-print()
-print("best_pathの時の報酬（コスト）")
-print(np.max(total_reward_list))
-print()
-print("平均")
-print(np.mean(total_reward_list))
-
-
-print("=========================================================================================================================================")
-print("re-culculate")
-total_reward_list = np.zeros(one_num * two_num * three_num * four_num * five_num)
-
-
-best_i = 0
-best_j = 0
-best_k = 0
-best_l = 0
-best_m = 0
-
-# print(G)
-reward_index_counter_replay = 0
-# print()するとdot形式で出力される
-for n in range(1):
-    for m in range(0, one_num):
-        for l in range(0, two_num):
-            for k in range(0, three_num):
-                for j in range(0, four_num):
-                    for i in range(0, five_num):
-                        # print("{}{}{}{}{}".format(m, l, k, j, i))
-                        if (reward_index_counter_replay == best_path):
-                            best_i = i
-                            best_j = j
-                            best_k = k
-                            best_l = l
-                            best_m = m
-                            res = gen_node_edge.combinatorial()
-                            res.result(best_path, reward_index_counter_replay, best_i, best_j, best_k, best_l, best_m)
-                            break
-                        else:
-
-                            # print("{}{}{}{}{}".format(path_list[n][m], path_list[m][l],path_list[l][k], path_list[k][j], path_list[j][i]))
-                            # print("{}".format(one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]))
-                            # total_reward_list[reward_index_counter] = one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]
-                            reward_index_counter_replay += 1
-
-
+# print("=========================================================================================================================================")
+# print("報酬の添字(終了時：one_num * two_num + three_num * four_num * five_num)")
+# print(reward_index_counter)
+# # print(total_reward_list)
 # print()
-# print(best_i)
-# print(best_j)
-# print(best_k)
-# print(best_l)
-# print(best_m)
-print("=========================================================================================================================================")
-print("報酬の添字(best_path と一致した時にforループを抜ける)")
-print(reward_index_counter_replay)
-# print(total_reward_list)
-print()
-print()
-
-
-G.render('image2')
-# binary_tree.<shape='   '>で保存
+# print()
+# print("best_path(インデックス)")
+# best_path = np.argmax(total_reward_list)
+# print(best_path)
+# print()
+# print("best_pathの時の報酬（コスト）")
+# print(np.max(total_reward_list))
+# print()
+# print("平均")
+# print(np.mean(total_reward_list))
+#
+#
+# print("=========================================================================================================================================")
+# print("re-culculate")
+# total_reward_list = np.zeros(one_num * two_num * three_num * four_num * five_num)
+#
+#
+# best_i = 0
+# best_j = 0
+# best_k = 0
+# best_l = 0
+# best_m = 0
+#
+# # print(G)
+# reward_index_counter_replay = 0
+# # print()するとdot形式で出力される
+# for n in range(1):
+#     for m in range(0, one_num):
+#         for l in range(0, two_num):
+#             for k in range(0, three_num):
+#                 for j in range(0, four_num):
+#                     for i in range(0, five_num):
+#                         # print("{}{}{}{}{}".format(m, l, k, j, i))
+#                         if (reward_index_counter_replay == best_path):
+#                             best_i = i
+#                             best_j = j
+#                             best_k = k
+#                             best_l = l
+#                             best_m = m
+#                             res = gen_node_edge.combinatorial()
+#                             res.result(best_path, reward_index_counter_replay, best_i, best_j, best_k, best_l, best_m)
+#                             break
+#                         else:
+#
+#                             # print("{}{}{}{}{}".format(path_list[n][m], path_list[m][l],path_list[l][k], path_list[k][j], path_list[j][i]))
+#                             # print("{}".format(one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]))
+#                             # total_reward_list[reward_index_counter] = one_list[m] * two_list[l] * three_list[k] * four_list[j] * five_list[i]
+#                             reward_index_counter_replay += 1
+#
+#
+# # print()
+# # print(best_i)
+# # print(best_j)
+# # print(best_k)
+# # print(best_l)
+# # print(best_m)
+# print("=========================================================================================================================================")
+# print("報酬の添字(best_path と一致した時にforループを抜ける)")
+# print(reward_index_counter_replay)
+# # print(total_reward_list)
+# print()
+# print()
+#
+#
+# G.render('image2')
+# # binary_tree.<shape='   '>で保存
